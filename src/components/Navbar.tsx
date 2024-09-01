d2b2d77feedcdfe671c902862f4a346cba63d152
MarkTuwen <m18698577021@163.com>
2024-09-01 22:03:47 +0800

    add login and register page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
 import MobileMenu from './MobileMenu'
+import { ClerkLoaded, ClerkLoading } from '@clerk/nextjs'
+import { SignedIn, SignedOut, SignIn, UserButton } from '@clerk/clerk-react'
 
 export default function Navbar() {
   return (
@@ -47,6 +51,29 @@ export default function Navbar() {
       </div>
       {/* RIGHT */}
       <div className="w-[30%] flex items-center justify-end gap-4 lg:gap-8">
+        <ClerkLoading>
+          <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <button>
+              <Image src="/people.png" alt="" width={20} height={20} />
+            </button>
+            <button>
+              <Image src="/messages.png" alt="" width={20} height={20} />
+            </button>
+            <button>
+              <Image src="/notifications.png" alt="" width={20} height={20} />
+            </button>
+            <UserButton/>
+          </SignedIn>
+          <SignedOut>
+            <div className='flex items-center gap-2 text-md'>
+              <Image src="/login.png" alt="" width={20} height={20} />
+              <Link href="/sign-in">Login/Register</Link>
+            </div>
+          </SignedOut>
+        </ClerkLoaded>
         <MobileMenu />
       </div>
     </div>
